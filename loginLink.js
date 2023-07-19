e8bdcc9da8680c54311b7f2d8a072e41944e9d3d
Simran Kaur <simran00kk@gmail.com>
2023-07-19 16:22:13 -0400

    first draft login link

--- loginLink.js
+++ loginLink.js
@@ -1,38 +1,33 @@
-/*
-When a user logs in to your application, you will need to set res.locals.username and res.locals.role to pass the username and role to the loginLink middleware. This middleware will 
+//Purpose: generate access tokens
 
-generate access and refresh tokens that will be stored in cookies and used for user authorization.
+/*Questions: 
+- Does it matter if the CRUD app uses tokens
+- i want to take time to think about what the tokens/authorization process is doing for us
+- should the token payload be the username? or the role?
+- how will this be verified? jwt.verify seems to check if an access token is real, but can that tell what type of access token they have? Do we need to knwo what type of access token they have, or will the verification process use something else (e.g.: res.locals) to determine what role the user has 
+- if we use role, will every USER have the same token? And would that be an issue?
+- how long do we want the access tokens to last (i.e.: what should the expiry be)
+- should we also use refresh tokens?
 */
+const loginLink = (req, res, next) => {
+  //require jwt token
+  const jwt = require("jsonwebtoken");
 
+  //res.locals.role will contain the role
+  //the below code would generate access tokens, with the payload being role
+  // but would it create a different token every time it is run?
+  //otherwise multiple Users may have the same token. that may or may not be an issue tho
+  const secretToken = `ACCESS_TOKEN_${res.locals.role.toUpperCase()}_SECRET`;
+  const accessToken = jwt.sign(res.locals.role, process.env[secretToken], {
+    expiresIn: "15min",
+  });
 
-//require dotenv.config?
-//require jwt token
-const jwt = require("jsonwebtoken");
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,
+  });
+  return next();
+};
 
-//safeQL had users save username in res.locals before this step, why?
-    //to assign a role
-    //let module-user eg 'rodrigo' determine the logic for deciding if a user is assigned whichever role,
-    //would they determine this based off of just username in req body? surely not- then anyone can claim to be admin rodrigo
-    //so these people have accounts and, in the prev step of middleware you would check the pw i assume
-
-//do you need to create different jwts depending on the role?
-//is the payload of each jwt a password? or is it a username? or is it the role?
-//are there difft secrets depending on the role? or just one secret for any kind of accesstoken (whether its read-only/read-and-write);
-
-
-//Note: ask rodrigo what the token would be named and if there is just one for access tokens nd just one for refresh tokens
-//also ask the team when these should expire
-//uncertain if the syntax for the 15min is correct
-
-//the below code would generate access tokens, with the payload being role? but would it create a different token every time it is run? otherwise multiple read-only may have the same token. that may or may not be an issue tho 
-
-const accessToken = jwt.sign(res.locals.role, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15min"})
-
-
-//normally you want to store refresh tokens in rediscache or db
-    //later will you check if that refresh token still exists in the db/cache 
-        //if not -send error?
-    //verify the token 
-
-
-//dont forget to save in cookies
+module.exports = { loginLink };
+//do i need to return next and how do i export this?
