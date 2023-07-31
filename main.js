c16d6596b6f1e9f1ea8f53a48e0879c12148493a
Simran Kaur <simran00kk@gmail.com>
2023-07-31 14:36:24 -0400

    old ver

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
 //hashing algo
 //payload
 //signature = hash payload + secret 
@@ -28,4 +5,3 @@ import { setupCounter } from './counter.js'
 // jwt.verify(token,secret) //
 
 // setupCounter(document.querySelector('#counter'))
-
