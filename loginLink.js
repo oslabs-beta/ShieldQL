855706880316a8eb167186bd68e3a3285c0f20fc
Joie Zhang <joie@princeton.edu>
2023-08-04 07:21:51 -0700

    Merge dev changes to loginLink file in customize-json branch

--- loginLink.js
+++ loginLink.js
@@ -1,22 +1,22 @@
 
 const loginLink = (req, res, next) => {
-  const jwt = require ('jsonwebtoken');
+  const jwt = require('jsonwebtoken');
 
   //create access token based on role passed via res.locals
   const secretToken = `ACCESS_TOKEN_${res.locals.role.toUpperCase()}_SECRET`;
-  try{
-    const accessToken = jwt.sign({role: res.locals.role}, process.env[secretToken], { expiresIn: '15000' });
+  try {
+    const accessToken = jwt.sign({ role: res.locals.role }, process.env[secretToken], { expiresIn: '15000' });
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: true,
     });
     return next();
   }
-  catch(err){
+  catch (err) {
     return next({
-        log: `Express error ${err}. ROLE NOT FOUND`,
-        status: 404,
-        message: { err: 'INVALID USER' }
+      log: `Express error ${err}. ROLE NOT FOUND`,
+      status: 404,
+      message: { err: 'INVALID USER' }
     })
   }
 };
