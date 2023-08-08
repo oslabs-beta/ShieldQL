1d95b0d32b13a13a3b214a864aef7f7b4a72b74f
rscalderon <samourcalderon@gmail.com>
2023-08-08 19:11:31 -0400

    more thorough pseucodode loginLink

--- loginLink.js
+++ loginLink.js
@@ -2,18 +2,20 @@ const jwt = require('jsonwebtoken');
 
 // loginLink is an express middleware function that verifies that the client making a graphQL query or mutation is authorized to do so through jwt verification
 const loginLink = (req, res, next) => {
-  //create access token based on role passed via res.locals
   const secretToken = `ACCESS_TOKEN_${res.locals.role.toUpperCase()}_SECRET`;
   try {
+    //create access token based on role passed via res.locals
     const accessToken = jwt.sign(
       { role: res.locals.role },
       process.env[secretToken],
       { expiresIn: '1d' }
     );
+    // set new cookie with access token
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: true,
     });
+    // move on to next step in middleware chain
     return next();
   } catch (err) {
     return next({
