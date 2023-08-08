970dda3751f4006cf4e2a7114602179d2e50c953
rscalderon <samourcalderon@gmail.com>
2023-08-08 19:12:33 -0400

    pseudocode main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { sanitizeQuery } = require('./sanitizeQuery');
 const { shieldqlConfig } = require('./shieldqlConfig');
 const { validateUser } = require('./validateUser');
 
+// our main.js file imports all of our library's functionality for export
 module.exports = {
   validateUser,
   loginLink,
